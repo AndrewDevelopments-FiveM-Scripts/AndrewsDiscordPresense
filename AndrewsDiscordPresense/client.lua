eaabbe688bdf12a3c5707a81be43010b711c4139
AndrewTheWusky <121670301+AndrewTheWusky@users.noreply.github.com>
2023-03-22 15:04:43 +1000

    Update client.lua

--- AndrewsDiscordPresense/client.lua
+++ AndrewsDiscordPresense/client.lua
@@ -1,34 +1,12 @@
 Citizen.CreateThread(function()
 	while true do
-        -- This is the Application ID (Replace this with you own)
 		SetDiscordAppId(config.applicationIdentification)
-
-        -- Here you will have to put the image name for the "large" icon.
 		SetDiscordRichPresenceAsset(config.logoImage)
-        
-        -- (11-11-2018) New Natives:
-
-        -- Here you can add hover text for the "large" icon.
         SetDiscordRichPresenceAssetText(config.logoSubtitle)
-       
-        -- Here you will have to put the image name for the "small" icon.
         SetDiscordRichPresenceAssetSmall(config.subImage)
-
-        -- Here you can add hover text for the "small" icon.
         SetDiscordRichPresenceAssetSmallText(config.subImageText)
-
-
-        -- (26-02-2021) New Native:
-
-        --[[ 
-            Here you can add buttons that will display in your Discord Status,
-            First paramater is the button index (0 or 1), second is the title and 
-            last is the url (this has to start with "fivem://connect/" or "https://") 
-        ]]--
         SetDiscordRichPresenceAction(0, config.ButtonName, config.buttonLink)
         SetDiscordRichPresenceAction(1, "Andrew Modifications", "https://discord.gg/4WY5p4jzMq")
-
-        -- It updates every minute just in case.
 		Citizen.Wait(60000)
 	end
 end)
