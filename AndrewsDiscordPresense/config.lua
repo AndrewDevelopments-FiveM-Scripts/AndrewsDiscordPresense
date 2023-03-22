5b78613cdc6265e35a1663f56adf96da821e5167
AndrewTheWusky <121670301+AndrewTheWusky@users.noreply.github.com>
2023-03-22 15:04:09 +1000

    Add files via upload

---
+++ AndrewsDiscordPresense/config.lua
@@ -0,0 +1,17 @@
+config = {}
+
+-- Thanks for downloading the resource.
+
+-- Application Config
+config.applicationIdentification = 000000000000000 -- Enter the identification number, aka "Application ID"
+
+-- Logo & Title config.
+config.logoImage = "bigImage" -- Enter the name of the image you uploaded to discord.com/developers.
+config.logoSubtitle = "Welcoming new members to our server." -- Enter a short subtitle for the main big image.
+config.subImage = "smallImage" -- Enter the name of the other image you uploaded to discord.com/developers.
+config.subImageText = "Currently Playing On: Server 1" -- Enter a short subtitle for the secondary small image.
+
+-- Button
+config.ButtonName = "Website" -- Enter the name of the button.
+config.ButtonLink = "http://andrewmodifications.xyz" -- Enter the link where you want it to go.
+
